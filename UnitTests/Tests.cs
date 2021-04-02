dea451c19fb1ed1087d34e1950f9bc6438f74501
Patrick Fankhauser <pazi@aol.at>
2021-04-02 21:59:13 +0200

    Add new Unit tests

--- UnitTests/Tests.cs
+++ UnitTests/Tests.cs
@@ -1,6 +1,7 @@
 using ChangeNowApi_V2;
 using ChangeNowApi_V2.Dto;
 using ChangeNowApi_V2.Enums;
+using ChangeNowApi_V2.Helper;
 using NUnit.Framework;
 using System.Threading;
 using System.Threading.Tasks;
@@ -15,6 +16,7 @@ namespace UnitTests
         /// Note! You have to use your own ApiKey
         /// </summary>
         private const string ApiKey = "96281cfe083eba5afec4afecf7859a1198e572a4f26f54f66a502085278de148";
+        private static string TransactionID = "eba706364c7e6c";
 
         [OneTimeSetUp]
         public void OneTimeSetup()
@@ -49,15 +51,15 @@ namespace UnitTests
             else
             {
                 request = new CurrencyRequest() { Active = active, Buy = buy, Flow = flow, Sell = sell };
-                
+
             }
 
 
             var result = await client.GetListOfAvailableCurrenciesAsync(request);
-            Assert.NotNull(result);           
+            Assert.NotNull(result);
             Assert.True(result.Count > 0);
-            
-          
+
+
         }
 
         [Test]
@@ -282,7 +284,7 @@ namespace UnitTests
         }
 
         [Test]
-        [TestCase(true, null, null, null, null,null,null, Flow.Standard,Direction.Direct, false)]
+        [TestCase(true, null, null, null, null, null, null, Flow.Standard, Direction.Direct, false)]
         [TestCase(false, "ltc", "doge", "10", null, null, null, Flow.Standard, Direction.Direct, false)]
         [TestCase(false, "eth", "btc", "0.1", "0.1", null, null, Flow.FixedRate, Direction.Reverse, false)]
         [TestCase(false, "btc", "eth", "0.2", null, null, null, Flow.FixedRate, Direction.Direct, true)]
@@ -414,7 +416,7 @@ namespace UnitTests
                 Assert.Null(result.ToAmount);
 
             }
-            else if(!objIsNull && useRateId)
+            else if (!objIsNull && useRateId)
             {
                 Assert.NotNull(result.FromCurrency);
                 Assert.NotNull(result.FromNetwork);
@@ -447,14 +449,15 @@ namespace UnitTests
             }
 
         }
-       
+
         [Test]
-        [TestCase(true, null,null,true)]
+        [TestCase(true, null, null, true)]
         [TestCase(false, "doge", "D6CkMBAGs7pB3DJ2Q5qYTYUwvg8MpTCazX", true)]
         [TestCase(false, "doge", "D6CkMBAGs7pB3DJ2Q5qYTYUwvg8MpTCaz", false)]
-        [TestCase(false, "dash", "XuvkS6AsfZ59LVUbEe4WDm26XFSPobvYwe", false)]
-        [TestCase(false, "btc", "388bmweLPNYxEj7ZYP1jBgvt38aoxvFbnn", true)]
+        [TestCase(false, "dash", "XuvkS6AsfZ59LVUbEe4WDm26XFSPobvYwe", true)]
+        [TestCase(false, "Btc", "388bmweLPNYxEj7ZYP1jBgvt38aoxvFbnn", true)]
         [TestCase(false, "eth", "0x1a747bc6e51161B51E7141B99825fdF5fb382266", true)]
+        [TestCase(false, "LTC", "M8jujfHsvvqai9Qe4qRevDKfU7nB83MRS5", true)]
         public async Task ValidateAddressAsyncTest(bool objIsNull, string currency, string address, bool expected)
         {
             AddressValidationRequest request;
@@ -486,16 +489,17 @@ namespace UnitTests
                 Assert.AreEqual(expected, result.Result);
             }
 
-           
+
         }
 
         [Test]
         [TestCase(true, null, null, true)]
         [TestCase(false, "doge", "D6CkMBAGs7pB3DJ2Q5qYTYUwvg8MpTCazX", true)]
         [TestCase(false, "doge", "D6CkMBAGs7pB3DJ2Q5qYTYUwvg8MpTCaz", false)]
-        [TestCase(false, "dash", "XuvkS6AsfZ59LVUbEe4WDm26XFSPobvYwe", false)]
-        [TestCase(false, "btc", "388bmweLPNYxEj7ZYP1jBgvt38aoxvFbnn", true)]
+        [TestCase(false, "dash", "XuvkS6AsfZ59LVUbEe4WDm26XFSPobvYwe", true)]
+        [TestCase(false, "Btc", "388bmweLPNYxEj7ZYP1jBgvt38aoxvFbnn", true)]
         [TestCase(false, "eth", "0x1a747bc6e51161B51E7141B99825fdF5fb382266", true)]
+        [TestCase(false, "LTC", "M8jujfHsvvqai9Qe4qRevDKfU7nB83MRS5", true)]
         public void ValidateAddressTest(bool objIsNull, string currency, string address, bool expected)
         {
             AddressValidationRequest request;
@@ -583,7 +587,7 @@ namespace UnitTests
         }
 
         [Test]
-        [TestCase(true, null,null,null,null,null)]
+        [TestCase(true, null, null, null, null, null)]
         [TestCase(false, "btc", "eth", "0.1", null, null)]
         public async Task GetMarketEstimatedInfosAsyncTest(bool objIsNull, string fromCurrency, string toCurrency, string fromAmount, string toAmount, string type)
         {
@@ -602,7 +606,7 @@ namespace UnitTests
                     FromAmount = fromAmount,
                     ToAmount = toAmount,
                     Type = type
-                    
+
                 };
             }
 
@@ -638,6 +642,286 @@ namespace UnitTests
             var result = client.GetMarketEstimatedInfos(request);
             Assert.NotNull(result);
 
+        }
+
+        [Test]
+        [TestCase(true, null, null, null, null)]
+        [TestCase(false, "ltc", "doge", "0.1", "D6CkMBAGs7pB3DJ2Q5qYTYUwvg8MpTCazX")]
+        public async Task CreateExchangeTransactionAsyncTest(bool objIsNull, string fromCurrency, string toCurrency, string fromAmount, string address)
+        {
+            TransactionRequest request;
+
+            if (objIsNull)
+            {
+                request = null;
+            }
+            else
+            {
+                request = new TransactionRequest()
+                {
+                    FromCurrency = fromCurrency,
+                    ToCurrency = toCurrency,
+                    FromAmount = fromAmount,
+                    Address = address,
+                    FromNetwork = fromCurrency,
+                    ToNetwork = toCurrency
+                };
+            }
+
+            var result = await client.CreateExchangeTransactionAsync(request);
+            Assert.NotNull(result);
+
+            if (objIsNull)
+            {
+                Assert.Null(result.PayinAddress);
+                Assert.Null(result.PayinExtraId);
+                Assert.Null(result.PayoutAddress);
+                Assert.Null(result.PayoutExtraId);
+                Assert.Null(result.PayoutExtraIdName);
+                Assert.Null(result.Id);
+                Assert.True(string.IsNullOrEmpty(result.Flow));
+                Assert.True(string.IsNullOrEmpty(result.FromCurrency));
+                Assert.True(string.IsNullOrEmpty(result.FromNetwork));
+                Assert.True(string.IsNullOrEmpty(result.Id));
+                Assert.True(string.IsNullOrEmpty(result.RefundAddress));
+                Assert.True(string.IsNullOrEmpty(result.RefundExtraId));
+                Assert.True(string.IsNullOrEmpty(result.ToCurrency));
+                Assert.True(string.IsNullOrEmpty(result.ToNetwork));
+            }
+            else
+            {
+                Assert.False(string.IsNullOrEmpty(result.PayinAddress));
+                Assert.Null(result.PayinExtraId);
+                Assert.False(string.IsNullOrEmpty(result.PayoutAddress));
+                Assert.True(string.IsNullOrEmpty(result.PayoutExtraId));
+                Assert.Null(result.PayoutExtraIdName);
+                Assert.False(string.IsNullOrEmpty(result.Id));
+                Assert.False(string.IsNullOrEmpty(result.Flow));
+                Assert.False(string.IsNullOrEmpty(result.FromCurrency));
+                Assert.False(string.IsNullOrEmpty(result.FromNetwork));
+                Assert.False(string.IsNullOrEmpty(result.Id));
+                Assert.True(string.IsNullOrEmpty(result.RefundAddress));
+                Assert.True(string.IsNullOrEmpty(result.RefundExtraId));
+                Assert.False(string.IsNullOrEmpty(result.ToCurrency));
+                Assert.False(string.IsNullOrEmpty(result.ToNetwork));
+            }
+
+            //Save id for the next test
+            if (!string.IsNullOrEmpty(result.Id))
+            {
+                TransactionID = result.Id;
+            }
+
+        }
+
+        [Test]
+        [TestCase(true, null, null, null, null)]
+        [TestCase(false, "ltc", "grs", "0.1", "3NrV5Lfu8Svejvppwhu3YdvMKKsEYxUd8L")]
+        public void CreateExchangeTransactionTest(bool objIsNull, string fromCurrency, string toCurrency, string fromAmount, string address)
+        {
+            TransactionRequest request;
+
+            if (objIsNull)
+            {
+                request = null;
+            }
+            else
+            {
+                request = new TransactionRequest()
+                {
+                    FromCurrency = fromCurrency,
+                    ToCurrency = toCurrency,
+                    FromAmount = fromAmount,
+                    Address = address,
+                    FromNetwork = fromCurrency,
+                    ToNetwork = toCurrency
+                };
+            }
+
+            var result = client.CreateExchangeTransaction(request);
+            Assert.NotNull(result);
+
+            if (objIsNull)
+            {
+                Assert.Null(result.PayinAddress);
+                Assert.Null(result.PayinExtraId);
+                Assert.Null(result.PayoutAddress);
+                Assert.Null(result.PayoutExtraId);
+                Assert.Null(result.PayoutExtraIdName);
+                Assert.Null(result.Id);
+                Assert.True(string.IsNullOrEmpty(result.Flow));
+                Assert.True(string.IsNullOrEmpty(result.FromCurrency));
+                Assert.True(string.IsNullOrEmpty(result.FromNetwork));
+                Assert.True(string.IsNullOrEmpty(result.Id));
+                Assert.True(string.IsNullOrEmpty(result.RefundAddress));
+                Assert.True(string.IsNullOrEmpty(result.RefundExtraId));
+                Assert.True(string.IsNullOrEmpty(result.ToCurrency));
+                Assert.True(string.IsNullOrEmpty(result.ToNetwork));
+            }
+            else
+            {
+                Assert.False(string.IsNullOrEmpty(result.PayinAddress));
+                Assert.Null(result.PayinExtraId);
+                Assert.False(string.IsNullOrEmpty(result.PayoutAddress));
+                Assert.True(string.IsNullOrEmpty(result.PayoutExtraId));
+                Assert.Null(result.PayoutExtraIdName);
+                Assert.False(string.IsNullOrEmpty(result.Id));
+                Assert.False(string.IsNullOrEmpty(result.Flow));
+                Assert.False(string.IsNullOrEmpty(result.FromCurrency));
+                Assert.False(string.IsNullOrEmpty(result.FromNetwork));
+                Assert.False(string.IsNullOrEmpty(result.Id));
+                Assert.True(string.IsNullOrEmpty(result.RefundAddress));
+                Assert.True(string.IsNullOrEmpty(result.RefundExtraId));
+                Assert.False(string.IsNullOrEmpty(result.ToCurrency));
+                Assert.False(string.IsNullOrEmpty(result.ToNetwork));
+            }
+        }
+
+        [Test]
+        [TestCase(true, true)]
+        [TestCase(false, true)]
+        [TestCase(false, false)]
+        public async Task GetTransactionStatusAsyncTest(bool objIsNull, bool function)
+        {
+            TransactionStatusRequest request = new TransactionStatusRequest() { Id = TransactionID };
+
+            TransactionStatusResponse result;
+            if (function)
+            {
+                if (objIsNull)
+                {
+                    request = null;
+                }
+                result = await client.GetTransactionStatusAsync(request);
+            }
+            else
+            {
+
+                result = await client.GetTransactionStatusAsync(TransactionID);
+
+            }
+
+            Assert.NotNull(result);
+
+            if (objIsNull)
+            {
+                Assert.False(result.ActionsAvailable);
+                Assert.Null(result.AmountFrom);
+                Assert.Null(result.AmountTo);
+                Assert.Null(result.CreatedAt);
+                Assert.Null(result.FromCurrency);
+                Assert.Null(result.ExpectedAmountFrom);
+                Assert.Null(result.PayoutAddress);
+            }
+            else
+            {
+                Assert.False(string.IsNullOrEmpty(result.PayinAddress));
+                Assert.False(result.ActionsAvailable);
+                Assert.False(string.IsNullOrEmpty(result.PayoutAddress));
+                Assert.NotNull(result.ExpectedAmountFrom);
+                Assert.False(string.IsNullOrEmpty(result.Status));
+                Assert.True(string.IsNullOrEmpty(result.PayinHash));
+                Assert.True(string.IsNullOrEmpty(result.PayoutHash));
+                Assert.False(string.IsNullOrEmpty(result.FromCurrency));
+                Assert.False(string.IsNullOrEmpty(result.ToCurrency));
+                Assert.Null(result.AmountTo);
+                Assert.Null(result.AmountFrom);
+                Assert.NotNull(result.ExpectedAmountTo);
+                Assert.Null(result.DepositReceivedAt);
+                Assert.NotNull(result.CreatedAt);
+            }
+
+
+        }
+
+        [Test]
+        [TestCase(true, true)]
+        [TestCase(false, true)]
+        [TestCase(false, false)]
+        public void GetTransactionStatusTest(bool objIsNull, bool function)
+        {
+            TransactionStatusRequest request = new TransactionStatusRequest() { Id = TransactionID };
+
+            TransactionStatusResponse result;
+            if (function)
+            {
+                if (objIsNull)
+                {
+                    request = null;
+                }
+                result = client.GetTransactionStatus(request);
+            }
+            else
+            {
+
+                result = client.GetTransactionStatus(TransactionID);
+
+            }
+
+            Assert.NotNull(result);
+
+            if (objIsNull)
+            {
+                Assert.False(result.ActionsAvailable);
+                Assert.Null(result.AmountFrom);
+                Assert.Null(result.AmountTo);
+                Assert.Null(result.CreatedAt);
+                Assert.Null(result.FromCurrency);
+                Assert.Null(result.ExpectedAmountFrom);
+                Assert.Null(result.PayoutAddress);
+            }
+            else
+            {
+                Assert.False(string.IsNullOrEmpty(result.PayinAddress));
+                Assert.False(result.ActionsAvailable);
+                Assert.False(string.IsNullOrEmpty(result.PayoutAddress));
+                Assert.NotNull(result.ExpectedAmountFrom);
+                Assert.False(string.IsNullOrEmpty(result.Status));
+                Assert.True(string.IsNullOrEmpty(result.PayinHash));
+                Assert.True(string.IsNullOrEmpty(result.PayoutHash));
+                Assert.False(string.IsNullOrEmpty(result.FromCurrency));
+                Assert.False(string.IsNullOrEmpty(result.ToCurrency));
+                Assert.Null(result.AmountTo);
+                Assert.Null(result.AmountFrom);
+                Assert.NotNull(result.ExpectedAmountTo);
+                Assert.Null(result.DepositReceivedAt);
+                Assert.NotNull(result.CreatedAt);
+            }
+
+
+        }
+
+        [Test]
+        [TestCase("new", TransactionStatus.New)]
+        [TestCase("Waiting", TransactionStatus.Waiting)]
+        [TestCase("confirming", TransactionStatus.Confirming)]
+        [TestCase("Exchanging", TransactionStatus.Exchanging)]
+        [TestCase("sending", TransactionStatus.Sending)]
+        [TestCase("finished", TransactionStatus.Finished)]
+        [TestCase("failed", TransactionStatus.Failed)]
+        [TestCase("Verifying", TransactionStatus.Verifying)]
+        [TestCase("refunded", TransactionStatus.Refunded)]
+        [TestCase("asdasd",TransactionStatus.UnKnown)]
+        [TestCase(null, TransactionStatus.UnKnown)]
+        public void ChangeNowConverterStringToEnumTests(string value, TransactionStatus expected)
+        {
+            Assert.AreEqual(expected, ChangeNowConverter.StringToTransactionStatus(value));
+        }
+
+        [Test]
+        [TestCase("new", TransactionStatus.New)]
+        [TestCase("waiting", TransactionStatus.Waiting)]
+        [TestCase("confirming", TransactionStatus.Confirming)]
+        [TestCase("exchanging", TransactionStatus.Exchanging)]
+        [TestCase("sending", TransactionStatus.Sending)]
+        [TestCase("finished", TransactionStatus.Finished)]
+        [TestCase("failed", TransactionStatus.Failed)]
+        [TestCase("verifying", TransactionStatus.Verifying)]
+        [TestCase("refunded", TransactionStatus.Refunded)]
+        [TestCase("unknown", TransactionStatus.UnKnown)]
+        public void ChangeNowConverterEnumToStringTest(string expected, TransactionStatus value)
+        {
+            Assert.AreEqual(expected, ChangeNowConverter.TransactionStatusToString(value));
         }
     }
 }
